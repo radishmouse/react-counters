f79b83785eb5797f19e486b32b121edf357ad4df
Chris Aquino <chris.aquino@gmail.com>
2018-07-18 09:34:58 -0400

    changes Counter to class component, with value of 3 hard coded

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,49 @@
 import React from 'react';
 
+// Rule #1: extend React.Component
+class Counter extends React.Component {
+  // Rule #2: must have render method
+  render() {
+    // Rule #3: must return some JSX
+    // or a call to React.createElement
+    return (
+      <div className='counter'>
+        3
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Counter is a React Component
-const Counter = ({
-  finalValue,
-  initialValue
-}) => {
-  // console.log(props);
-  return (
-    <div className='counter'>
-      {initialValue}
-    </div>
-  );
-};
+// const Counter = ({
+//   finalValue,
+//   initialValue
+// }) => {
+//   // console.log(props);
+//   return (
+//     <div className='counter'>
+//       {initialValue}
+//     </div>
+//   );
+// };
 
 export default Counter;
