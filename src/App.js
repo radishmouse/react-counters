fc56d7ef0cce5aaad7e0324aa6e825f23ab46ba2
Chris Aquino <chris.aquino@gmail.com>
2018-07-18 11:15:09 -0400

    converts app to class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Counter from './Counter';
 
-const handleClick = () => {
-  console.log('haaaaay');
-};
 
 function convertNumToCounter(value) {
   return (
@@ -12,24 +9,51 @@ function convertNumToCounter(value) {
 }
 
 // This is not *quite* the React way.
-let counterValues = [42, 3, 18, 29, 56, 11];
-let counterElements = counterValues.map(convertNumToCounter);
+// let counterValues = [42, 3, 18, 29, 56, 11];
+// let counterElements = counterValues.map(convertNumToCounter);
 
 /*
 let counterElements = counterValues.map(value => <Counter initialValue={value} />);
 */
 
-// App is a React Component
-const App = () => {
-  return (
-    <div className='container'>
-      <button onClick={handleClick}>+</button>
-      <div className='counter-box'>
-        {counterElements}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counterValues: [33, 11, 2]
+    };
+  }
+
+  _handleClick = () => {
+    console.log('haaaaay lalalalalalalalalallalalalalalala');
+    this.setState({
+      counterValues: this.state.counterValues.concat(0)
+    });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <button onClick={this._handleClick}>+</button>
+        <div className='counter-box'>
+          {this.state.counterValues.map(convertNumToCounter)}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// App is a React Component
+// const App = () => {
+//   return (
+//     <div className='container'>
+//       <button onClick={handleClick}>+</button>
+//       <div className='counter-box'>
+//         {counterElements}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
 
